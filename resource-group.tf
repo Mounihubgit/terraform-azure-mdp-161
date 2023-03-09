9fa2f2d00d9fa31c3e19f284d44c22c9662dc45a
ravi2krishna <ravi2krishna@gmail.com>
2023-03-09 02:54:46 +0000

    web resource group with tags

---
+++ resource-group.tf
@@ -0,0 +1,9 @@
+# Resource Group For Web App Project
+resource "azurerm_resource_group" "web-rg" {
+  name     = "web-rg"
+  location = "East US"
+    tags = {
+    env = "dev"
+    tier = "frontend"
+  }
+}
